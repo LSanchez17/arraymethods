4bbfe6329dccfbd11478237b7850cb7225ba36cf
LSanchez17 <galaxyplayeremail@gmail.com>
2020-04-25 23:34:53 -0400

    via upload
    find and findindex exercises

---
+++ findFindIndex/find-findindex.js
@@ -0,0 +1,58 @@
+/* 
+Write a function called `findUserByUsername` which accepts an array of objects, 
+each with a key of username, and a string. The function should return the first 
+object with the key of username that matches the string passed to the function. 
+If the object is not found, return undefined. 
+
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+console.log(findUserByUsername(users, 'mlewis')) // {username: 'mlewis'}
+console.log(findUserByUsername(users, 'taco'))
+*/
+const findUserByUsername = (usersArray, username) => {
+  return usersArray.find(val => val.username === username)
+}
+
+
+/*
+Write a function called `removeUser` which accepts an array of objects, 
+each with a key of username, and a string. The function should remove the 
+object from the array. If the object is not found, return undefined. 
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+removeUser(users, 'akagen') // {username: 'akagen'}
+removeUser(users, 'akagen') // undefined
+*/
+
+const removeUser = (usersArray, username) => {
+  let deletedUserIndex = usersArray.findIndex(val => val.username === username);
+  let deletedUser;
+
+  if(deletedUserIndex !== -1){
+    deletedUser = usersArray[deletedUserIndex];
+    usersArray.splice(deletedUserIndex, 1);
+    return deletedUser;
+  }
+  else{
+    return undefined;
+  }
+}
+
+const users = [
+  {username: 'mlewis'},
+  {username: 'akagen'},
+  {username: 'msmith'}
+];
+
+console.log(removeUser(users, 'akagen')) // {username: 'akagen'}
+console.log(removeUser(users, 'akagen'))
